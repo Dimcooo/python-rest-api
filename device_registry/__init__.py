afea5dd968f34b26156379843cde34f9c8b95e0e
dimco <dimcoyk@gmail.com>
2019-07-10 13:43:50 +0300

    Deleted excess imports

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -3,7 +3,7 @@ import shelve
 import os
 
 from flask import Flask, g, jsonify, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
